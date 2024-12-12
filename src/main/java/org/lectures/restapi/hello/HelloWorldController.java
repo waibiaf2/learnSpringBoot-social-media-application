6354d6898e316e76e011a55dc71832ff135de31b
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-12-12 13:42:10 +0300

    Implement internationalization

--- src/main/java/org/lectures/restapi/hello/HelloWorldController.java
+++ src/main/java/org/lectures/restapi/hello/HelloWorldController.java
@@ -1,11 +1,21 @@
 package org.lectures.restapi.hello;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World!";
@@ -19,5 +29,15 @@ public class HelloWorldController {
     @GetMapping("/hello-world/{name}")
     public HelloWorldBean helloWorldWithPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s!", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage(
+            "good.morning.message",
+            null,
+            "Default Message",
+            locale);
     }
 }
