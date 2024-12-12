d6efa96c9b4c60495e86795f7f8f603220daef1b
Waibi Andrew Franklin <waibiaf2@gmail.com>
2024-12-12 16:07:46 +0300

    Add filtering example with SomeBean and FilteringController
    Introduced `SomeBean` with `@JsonIgnore` to exclude field2 from API responses; static filtering. Added `FilteringController` with a `/filtering` endpoint that returns a sample `SomeBean` object. This demonstrates basic field filtering in REST API responses.

---
+++ src/main/java/org/lectures/restapi/filtering/FilteringController.java
@@ -0,0 +1,13 @@
+package org.lectures.restapi.filtering;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+    @GetMapping("/filtering")
+    public SomeBean filtering() {
+        return new SomeBean("value1", "value2", "value3");
+    }
+}
+
